e9235ee5e78c7aaa34265437955d69635511749e
Daniel Kowalec <armyguyinfl@users.noreply.github.com>
2022-10-06 10:59:32 -0400

    Lab 2 complete.

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,26 @@
-const typeDefs = require("./schema");
+const typeDefs = require('./schema');
 
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require('apollo-server');
 
-const server = new ApolloServer({ typeDefs, mocks: true });
+const mocks = {
+  Query: () => ({
+    coders: () => [...new Array(6)],
+  }),
+  Activity: () => ({
+    id: () => '1',
+    name: () => 'Coding',
+    description: () => 'Coding is fun',
+  }),
+  Coder: () => ({
+    id: () => '1',
+    name: () => 'A coder who codes.',
+    description: () => 'A coder who codes.',
+    activities: () => [1],
+  }),
+};
+
+const server = new ApolloServer({ typeDefs, mocks });
 
 server.listen().then(() => {
-  console.log("Server is running at http://localhost:4000");
+  console.log('Server is running at http://localhost:4000');
 });
