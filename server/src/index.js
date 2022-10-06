a2abb2d6c77c57f025ae4d00296460d48433e157
Daniel Kowalec <armyguyinfl@users.noreply.github.com>
2022-10-06 10:37:59 -0400

    Lab 1 complete.

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const typeDefs = require("./schema");
 
 const { ApolloServer } = require("apollo-server");
 
-const server = new ApolloServer({});
+const server = new ApolloServer({ typeDefs, mocks: true });
 
 server.listen().then(() => {
   console.log("Server is running at http://localhost:4000");
